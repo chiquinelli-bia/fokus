b2f7e709f6a88a00e6d4c63a064406314e694b65
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-06-19 20:14:51 -0300

    atualizar tempo do timer quando contexto for foco

--- scripts/contexto.js
+++ scripts/contexto.js
@@ -1,7 +1,7 @@
 import { btn, html, img, titulo } from "./dom.js";
 import { mostrarTempo } from './timer.js';
 export const TEMPOS = {
-  foco: 30,
+  foco: 1500,
   'descanso-curto': 300,
   'descanso-longo': 900
 };
